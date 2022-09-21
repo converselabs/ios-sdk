f9b07c19bd24d0a75fa3e68960663c0e1a31c02f
Ankit <angraankit@gmail.com>
2022-09-20 23:12:12 -0400

    Initial commit

---
+++ DeepConverse/DeepConverse/DeepConverse.h
@@ -0,0 +1,18 @@
+//
+//  DeepConverse.h
+//  DeepConverse
+//
+//  Created by Ankit Angra on 2022-09-20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeepConverse.
+FOUNDATION_EXPORT double DeepConverseVersionNumber;
+
+//! Project version string for DeepConverse.
+FOUNDATION_EXPORT const unsigned char DeepConverseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeepConverse/PublicHeader.h>
+
+
---
+++ DeepConverse/DeepConverse/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  DeepConverse
+//
+//  Created by Ankit Angra on 2022-09-20.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Do something"
+    }
+    
+}

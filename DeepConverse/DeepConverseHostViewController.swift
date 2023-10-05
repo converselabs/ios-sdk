1ac568ec3213e6c62be3864517d5c95e07393536
Dhruv Arya <silent-vim@users.noreply.github.com>
2023-10-04 17:27:46 -0700

    Update DeepConverseHostViewController.swift

--- DeepConverse/DeepConverseHostViewController.swift
+++ DeepConverse/DeepConverseHostViewController.swift
@@ -2,32 +2,34 @@
 //  DeepConverseHostViewController.swift
 //  DeepConverse
 //
-//  Created by Ankit Angra on 2022-09-22.
 //
 
 import UIKit
 import WebKit
 
 class DeepConverseHostViewController: UIViewController {
-    
+
     private var webview: WKWebView!
     private var url: URL!
     private var timeout: Double!
     private var delegate: DeepConverseDelegate!
-    
+    private var session: DeepConverseSDKSession!
+
     struct ChatbotActions: Codable {
         var action : String
     }
-    
+
     static func createWebController(with url: URL,
+                                    session: DeepConverseSDKSession,
                                     timeout: Double,
                                     delegate: DeepConverseDelegate) -> DeepConverseHostViewController {
         let bundle = Bundle(for: DeepConverseHostViewController.self)
-        
-        print("Webview URL:", url)
-        
+
+        print("[DeepConverseSDK] Webview URL:", url)
+        print("[DeepConverseSDK] Metadata: ", session.metadata)
+
         var storyboard:UIStoryboard
-        
+
         if (bundle.path(forResource: "DeepConverse", ofType: "bundle") != nil){
             let frameworkBundlePath = bundle.path(forResource: "DeepConverse", ofType: "bundle")!
             let frameworkBundle = Bundle(path: frameworkBundlePath)
@@ -35,28 +37,53 @@ class DeepConverseHostViewController: UIViewController {
         } else {
             storyboard = UIStoryboard(name: "DeepConverseHostViewController", bundle: bundle)
         }
-        
+
         guard let viewController = storyboard.instantiateInitialViewController() as? DeepConverseHostViewController else { fatalError("This should never, ever happen.") }
         viewController.url = url
         viewController.timeout = timeout
         viewController.delegate = delegate
-        
+        viewController.session = session
         return viewController
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(DeepConverseHostViewController.keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
         configureWebview()
     }
-    
+
     override func viewDidDisappear(_ animated: Bool) {
         self.delegate.didCloseBot()
         super.viewDidDisappear(animated)
     }
-    
+
+    private func json(from object:Any) -> String? {
+        guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else {
+            return nil
+        }
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+
     private func actionButtonJs() -> String {
+        do {
+            var metadataJSON = json(from: self.session.metadata)
+            print("[DeepConverseSDK] Metadata:", metadataJSON)
+            let s = """
+        setTimeout(function () {var evt = new CustomEvent('botWidgetInit', { detail: \(metadataJSON!) });document.dispatchEvent(evt);}, 100)
+
+        document.addEventListener('dc.bot', function(e) {
+          let payload = { action: e.detail.action };
+          window.webkit.messageHandlers.actionTapped.postMessage(payload);
+        });
+        """
+            return s;
+        } catch {
+            print("[DeepConverseSDK] Error in Metadata" + error.localizedDescription);
+        }
+
         let s = """
+        setTimeout(function () {var evt = new CustomEvent('botWidgetInit', { detail: {} });document.dispatchEvent(evt);}, 100)
+
         document.addEventListener('dc.bot', function(e) {
           let payload = { action: e.detail.action };
           window.webkit.messageHandlers.actionTapped.postMessage(payload);
@@ -64,9 +91,9 @@ class DeepConverseHostViewController: UIViewController {
         """
         return s;
     }
-    
+
     private func configureWebview() {
-        
+
         let webConfiguration = WKWebViewConfiguration()
         let contentController = WKUserContentController()
         let js: String = actionButtonJs();
@@ -81,12 +108,12 @@ class DeepConverseHostViewController: UIViewController {
 
         self.webview = WKWebView(frame: self.view.frame, configuration: webConfiguration)
         self.view.addSubview(self.webview)
-        
+
         self.webview.scrollView.isScrollEnabled = false
         let webRequest = URLRequest(url: url,
                                     cachePolicy: .useProtocolCachePolicy,
                                     timeoutInterval: timeout)
-        
+
         let layoutGuide = self.view.safeAreaLayoutGuide
         self.webview.translatesAutoresizingMaskIntoConstraints = false
         self.webview.leadingAnchor.constraint(
@@ -97,9 +124,9 @@ class DeepConverseHostViewController: UIViewController {
               equalTo: layoutGuide.topAnchor).isActive = true
         self.webview.bottomAnchor.constraint(
               equalTo: layoutGuide.bottomAnchor).isActive = true
-        
+
         self.webview.load(webRequest)
-        
+
         DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
             if (self.webview.isLoading) {
                 self.webview.stopLoading()
@@ -115,25 +142,25 @@ extension DeepConverseHostViewController : WKScriptMessageHandler {
         didReceive message: WKScriptMessage
     ) {
         do {
-            print("message:", message.body);
+            print("[DeepConverseSDK] message:", message.body);
             guard let payload = message.body as? [String: String] else { return }
             print("struct:", payload["action"])
-            
+
             switch (payload["action"]) {
             case "open":
-                print("open action");
+                print("[DeepConverseSDK] open action");
                 break;
             case "minimize":
-                print("minimize action")
+                print("[DeepConverseSDK] minimize action")
                 self.dismiss(animated: true, completion: nil);
                 break;
             default:
-                print("unknown action")
+                print("[DeepConverseSDK] unknown action")
             }
-            
+
             delegate.didReceiveEvent(event: payload)
         } catch {
-            print("DeepConverse event error")
+            print("[DeepConverseSDK] Event error")
         }
     }
 }

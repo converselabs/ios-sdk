96d220d3f6cbd8471f4f7e2c8d5124d9d87bcfaf
Dhruv Arya <silent-vim@users.noreply.github.com>
2023-10-04 17:28:27 -0700

    Update DeepConverseSDK.swift

--- DeepConverse/DeepConverseSDK.swift
+++ DeepConverse/DeepConverseSDK.swift
@@ -1,8 +1,6 @@
 //
-//  BotManager.swift
 //  DeepConverse
 //
-//  Created by Ankit Angra on 2022-09-21.
 //
 
 import Foundation
@@ -22,14 +20,14 @@ public protocol DeepConverseDelegate {
 }
 
 public class DeepConverseSDKSession {
-    
+
     var subDomain: String?
     var botName: String?
     var metadata: [String: String]?
     var openWebLinkInSafari: Bool
     var webViewTimeout: Double
     var navigationBarOpeque: Bool
-    
+
     public init (
         subDomain: String,
         botName: String,
@@ -49,74 +47,70 @@ public class DeepConverseSDKSession {
 
 
 public class DeepConverseSDK {
-    
+
     private var delegate: DeepConverseDelegate
     private var botUrl: URL?
     private var timeout: Double
-    
+    private var session: DeepConverseSDKSession
+
     public init(delegate: DeepConverseDelegate, session: DeepConverseSDKSession) {
         self.delegate = delegate
         self.timeout = session.webViewTimeout
+        self.session = session
         createSession(session: session)
     }
-    
+
     private func createSession (
         session: DeepConverseSDKSession
     ) {
-        
+
         guard let subDomain = session.subDomain,
               let botName = session.botName,
               let context = session.metadata else {
             delegate.didWebViewFail(withError: DeepConverseWebHostError.SDKInitilizationError)
             return
         }
-        
+
         guard let url = createUrl (
             subdomain: subDomain,
-            botName: botName,
-            context: context
+            botName: botName
         ) else {
             return
         }
-        
+
         botUrl = url
     }
-    
+
     private func createUrl(
         subdomain: String,
-        botName: String,
-        context: [String: String]
+        botName: String
     ) -> URL? {
-        
-        let urlString: String = "https://cdn.converseapps.com/v1/assets/widget/embedded-chatbot"
-        
+
+        let urlString: String = "https://cdn.deepconverse.com/v1/assets/widget/embedded-chatbot"
+
         guard let urlCompenent = NSURLComponents(string: urlString) else {
             return nil
         }
-        
+
         var queryItems = [URLQueryItem]()
 
         let config = subdomain + "-" + botName + ".deepconverse.com"
         let hostnameQI = URLQueryItem.init(name: "hostname", value: config)
         queryItems.append(hostnameQI)
-        
-        for item in context {
-            let queryItem = URLQueryItem.init(name: item.key, value: item.value)
-            queryItems.append(queryItem)
-        }
-        
+
         urlCompenent.queryItems = queryItems
-        
+
         return urlCompenent.url
     }
-    
+
     public func openBot(viewController: UIViewController) {
-        
+
         guard let url = botUrl else {
             fatalError("Something went wrong. Please check your session")
         }
-        
+
         let vc = DeepConverseHostViewController.createWebController(with: url,
+                                                                    session: session,
                                                                     timeout: timeout,
                                                                     delegate: delegate)
         viewController.present(vc, animated: true) {
@@ -124,8 +118,3 @@ public class DeepConverseSDK {
         }
     }
 }
-
-
-
-
-
